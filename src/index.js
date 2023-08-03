bdea7b4d691a9046a310c88746a8032de559b743
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-08-03 23:37:06 +0530

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+//ReactDOM.render('kya dikhana hai','kaha dikhana hai', 'call back');
+ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"));
