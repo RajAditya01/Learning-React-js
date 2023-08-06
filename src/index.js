3d1ad951c71f0666c6a2d871a3413c289d0ddba6
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-08-06 16:33:53 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // ReactDOM.render('kya dikhana hai', 'kaha dikhana hai', 'call back');
-ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"));
+//ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"));
 
 //IN React v16 its possible for render() to return and array elements.
 
@@ -12,7 +12,6 @@ ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"));
 //         <h2>Wellcomme to my react course</h2>
 //         <p> I am aditya Raj</p> </div>,
 //     document.getElementById("root"));
-
 
 // Using ReactDOM.render to render an array of elements
 // ReactDOM.render(
@@ -25,16 +24,32 @@ ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"));
 // );
 
 //React fragment
+// ReactDOM.render(
+//     <>
+//         <h1 key="heading">Hello world!</h1>
+//         <h2 key="subheading">Welcome to my React course</h2>
+//         <p key="paragraph">I am Aditya Raj</p>
+//     </>,
+//     document.getElementById("root")
+// );
+
+//challenge 1
 ReactDOM.render(
     <>
-        <h1 key="heading">Hello world!</h1>
-        <h2 key="subheading">Welcome to my React course</h2>
-        <p key="paragraph">I am Aditya Raj</p>
+        <h1>Aditya Netflix picks</h1>
+        <h3>List of 5 best series</h3>
+        <p>
+            1. Dark <br />
+            2. Extra curricular <br />
+            3. My holo love <br />
+            4. My first 2 love <br />
+            5. Mr Robot
+        </p>
     </>,
     document.getElementById("root")
 );
 
-// 
+//
 // var h1 = document.createElement("h1");
 // h1.innerHTML = "Aditya raj";
 // document.getElementById("root").appendChild(h1);
