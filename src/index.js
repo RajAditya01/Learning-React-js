7b1c4463eeffab4a069581d7930d32fcbce7fbe9
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-08-06 16:02:54 +0530

    .

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 
-//ReactDOM.render('kya dikhana hai','kaha dikhana hai', 'call back');
+// ReactDOM.render('kya dikhana hai', 'kaha dikhana hai', 'call back');
 ReactDOM.render(<h1>Hello world!</h1>, document.getElementById("root"));
 
-//2
-var h1=document.createElement("h1");
-h1.innerHTML="Aditya raj";
-document.getElementById("root").apendChild(h1);
+// 2
+var h1 = document.createElement("h1");
+h1.innerHTML = "Aditya raj";
+document.getElementById("root").appendChild(h1);
