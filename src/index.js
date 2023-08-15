84d5feb153d5b5dd39fe00e25727904a0b8cca78
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-08-15 16:40:03 +0530

    .

--- src/index.js
+++ src/index.js
@@ -358,17 +358,47 @@ ReactDOM.render(
 {/* khud ka custom attribute */}
 
 <Card
-imgsrc=""
+imgsrc="https://w0.peakpx.com/wallpaper/602/919/HD-wallpaper-dark-netflix-serie-dark-netflix-serie.jpg"
 title="A Netflix Original Series"
 sname="DARK"
 link="https://www.netflix.com/watch/80114790?trackId=255824129"
 />
 
-<Card/>
-<Card/>
-<Card/>
-<Card/>
-<Card/>
+<Card
+imgsrc="https://i.pinimg.com/originals/0b/a4/43/0ba4436f3d23254fcae9369bccebd73d.jpg"
+title="A Netflix Original Series"
+sname="STRANGER THINGS"
+link="https://www.netflix.com"
+/>
+
+<Card
+imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-WzkdAEToGSzj1epHTopsy7LxluCedOulsF4jDB9BMtGeM_EFxcerNYmoq3xHyofJbYs&usqp=CAU"
+title="A Netflix Original Series"
+sname="DARE DEVIL"
+link="https://www.netflix.com/watch/80114790?trackId=255824129"
+/>
+
+<Card
+imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNOCcu-GOlXniHpjNrT4GYrETi-DTKFGqoyr5HuUvjQWF8NnV8y9e0STfLUevCb_OBT20&usqp=CAU"
+title="A Netflix Original Series"
+sname="MONEY HEIST"
+link="https://www.netflix.com"
+/>
+
+<Card
+imgsrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhkf7eutOJF0IMVdW2Ugp-s4QOjnvgDH5WqWjwn9upS8J_a6HFLBjpxQW_YX397qvpAZo&usqp=CAU"
+title="A Netflix Original Series"
+sname="HEAVEN"
+link="https://www.netflix.com"
+/>
+
+<Card
+imgsrc="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/thecrown-1527611295.jpg"
+title="A Netflix Original Series"
+sname="CROWN"
+link="https://www.netflix.com"
+/>
+
 </>,
     document.getElementById('root')
 );
