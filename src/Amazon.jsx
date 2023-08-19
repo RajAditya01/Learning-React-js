e9868323535e6633cbac5c654790a957ce0914dd
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2023-08-19 09:56:27 +0530

    .

--- src/Amazon.jsx
+++ src/Amazon.jsx
@@ -1,1 +1,16 @@
-c
+import React from "react"
+import Sdata from "./Sdata";
+import Card from "./card";
+
+const Amazon=()=>{
+    return(
+        <Card
+            imgsrc={Sdata[3].imgsrc}
+            title={Sdata[3].title}
+            sname={Sdata[3].sname}
+            link={Sdata[3].link}
+        />
+    );
+}
+
+export default Netflix;
